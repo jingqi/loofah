11dfcb8b8abcaa00e0ea0b9dc18dee3a99e5bb1d
jingqi <jingqi.se@gmail.com>
2017-01-06 16:55:18 +0800

    添加一个 loofah.h 导出全部接口

---
+++ src/loofah/loofah.h
@@ -0,0 +1,35 @@
+﻿
+#ifndef ___HEADFILE_6D81A172_D34D_4AEC_A0F5_89031A4E6E57_
+#define ___HEADFILE_6D81A172_D34D_4AEC_A0F5_89031A4E6E57_
+
+#include "loofah_config.h"
+
+// base
+#include "inet_base/inet_endian.h"
+#include "inet_base/inet_addr.h"
+#include "inet_base/sock_base.h"
+#include "inet_base/sock_stream.h"
+#include "inet_base/channel.h"
+#include "inet_base/connector.h"
+#include "inet_base/utils.h"
+
+// reactor
+#include "reactor/react_handler.h"
+#include "reactor/react_channel.h"
+#include "reactor/react_acceptor.h"
+#include "reactor/react_connector.h"
+#include "reactor/reactor.h"
+
+// proactor
+#include "proactor/io_request.h"
+#include "proactor/proact_handler.h"
+#include "proactor/proact_channel.h"
+#include "proactor/proact_acceptor.h"
+#include "proactor/proact_connector.h"
+#include "proactor/proactor.h"
+
+// package channel
+#include "package/package.h"
+#include "package/package_channel.h"
+
+#endif
