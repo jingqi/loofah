f8a8643d1d2b584035b57f857698878996cec6e7
jingqi <jingqi.se@gmail.com>
2019-10-10 00:52:24 +0800

    更新 nut 库

--- proj/makefile/test-loofah.mk
+++ proj/makefile/test-loofah.mk
@@ -1,32 +1,47 @@
-#!/user/bin/env make
+#!/usr/bin/env make
 
+# Preface
+include ${NUT_PATH}/proj/makefile/mkinclude/vars.mk
+include ${NUT_PATH}/proj/makefile/mkinclude/funcs.mk
+
+# Project vars
 TARGET_NAME = test-loofah
 SRC_ROOT = ../../src/${TARGET_NAME}
+OBJ_ROOT = ${OUT_DIR}/obj/${TARGET_NAME}
+TARGET = ${OUT_DIR}/${TARGET_NAME}
 
-# Preface rules
-include ${NUT_PATH}/proj/makefile/preface_rules.mk
-
-# Includes
-CPPFLAGS += -I${SRC_ROOT}/.. -I${NUT_PATH}/src
-
-# Defines
-CPPFLAGS +=
+# Make dirs
+$(call make_image_dir_tree,${SRC_ROOT},${OBJ_ROOT})
 
 # C/C++ standard
 CFLAGS += -std=c11
 CXXFLAGS += -std=c++11
 
-# Libraries
+# Defines and flags
+
+# Includes
+
+# Files
+SRCS = $(call files,${SRC_ROOT},*.c *.cpp)
+OBJS = $(patsubst ${SRC_ROOT}%.cpp,${OBJ_ROOT}%.o,$(patsubst ${SRC_ROOT}%.c,${OBJ_ROOT}%.o,${SRCS}))
+DEPS = $(patsubst ${SRC_ROOT}%.cpp,${OBJ_ROOT}%.d,$(patsubst ${SRC_ROOT}%.c,${OBJ_ROOT}%.d,${SRCS}))
+
+# Library loofah
+CPPFLAGS += -I${SRC_ROOT}/..
+LDFLAGS += -L${OUT_DIR} -lloofah
+LIB_DEPS += ${OUT_DIR}/libloofah.${DL_SUFFIX}
+
+# Library nut
+CPPFLAGS += -I${NUT_PATH}/src
+LDFLAGS += -L${OUT_DIR} -lnut
+LIB_DEPS += ${OUT_DIR}/libnut.${DL_SUFFIX}
+
+# Other libraries
 ifeq (${HOST}, Linux)
 	LDFLAGS += -lpthread -latomic
 endif
-LDFLAGS += -L${OUT_DIR} -lnut -lloofah
-LIB_DEPS += ${OUT_DIR}/libnut.${DL_SUFFIX} \
-			${OUT_DIR}/libloofah.${DL_SUFFIX}
 
-# TARGET
-TARGET = ${OUT_DIR}/${TARGET_NAME}
-
+# Targets
 .PHONY: all clean rebuild
 
 all: ${TARGET}
@@ -38,15 +53,15 @@ rebuild:
 	$(MAKE) -f test-loofah.mk clean
 	$(MAKE) -f test-loofah.mk all
 
-${NUT_PATH}/proj/makefile/${OUT_DIR_NAME}/libnut.${DL_SUFFIX}:
+${OUT_DIR}/libloofah.${DL_SUFFIX}: FORCE
+	$(MAKE) -f loofah.mk
+
+${NUT_PATH}/proj/makefile/${OUT_DIR_NAME}/libnut.${DL_SUFFIX}: FORCE
 	cd ${NUT_PATH}/proj/makefile ; $(MAKE) -f nut.mk
 
 ${OUT_DIR}/libnut.${DL_SUFFIX}: ${NUT_PATH}/proj/makefile/${OUT_DIR_NAME}/libnut.${DL_SUFFIX}
 	cp -f $< $@
 
-${OUT_DIR}/libloofah.${DL_SUFFIX}:
-	$(MAKE) -f loofah.mk
-
 # Rules
-include ${NUT_PATH}/proj/makefile/common_rules.mk
-include ${NUT_PATH}/proj/makefile/app_rules.mk
+include ${NUT_PATH}/proj/makefile/mkinclude/common_rules.mk
+include ${NUT_PATH}/proj/makefile/mkinclude/app_rules.mk
